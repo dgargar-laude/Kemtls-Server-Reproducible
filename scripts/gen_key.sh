473dc0e6257b729bdbf7dd5b5c88c19c85b4c90e
Ruben Gonzalez <rg@ht11.org>
2022-01-18 13:24:03 +0100

    Add certgen script

--- scripts/gen_key.sh
+++ scripts/gen_key.sh
@@ -1,6 +1,34 @@
-export ROOT_SIGALG=Falcon512
-export LEAF_SIGALG=Falcon512
-export KEX_ALG=lightsaber
+#!/bin/bash
+
+set -o nounset
+set -o errexit
+
 
 cd /mnt/kemtls-experiment/mk-cert
-python3 encoder.py
+cp /mnt/updated/encoder.py .
+
+SIG_ALGS="falcon512 dilithium2 rainbowIclassic"
+KEX_ALGS="kyber512 ntruhps2048509 lightsaber"
+TARGET_DIR="certs"
+
+if [ ! -d  $TARGET_DIR ]; then
+    mkdir -p $TARGET_DIR
+fi
+
+for SIG_ALG in $SIG_ALGS; do
+    for KALG in $KEX_ALGS; do
+
+        export ROOT_SIGALG=$SIG_ALG
+        export INT_SIGALG=$SIG_ALG
+        export LEAF_ALG=$KALG
+
+        for i in {1..1000}; do
+            NUM=$(printf "%04d" $i)
+            echo "Doing ${ROOT_SIGALG} x ${LEAF_ALG} number ${NUM} now."
+            python3 encoder.py
+            mv kem.crt ${TARGET_DIR}/${ROOT_SIGALG}_${LEAF_ALG}_${NUM}.crt
+            mv kem.key ${TARGET_DIR}/${ROOT_SIGALG}_${LEAF_ALG}_${NUM}.key
+            mv kem-ca.crt ${TARGET_DIR}/${ROOT_SIGALG}_${LEAF_ALG}_${NUM}_ca.crt
+        done
+    done
+done
